e6478f8e6db207de6c2546c180689141fb893225
eMul <e2016141010@gmail.com>
2020-12-01 00:28:06 +0700

    fix session

--- application/config/config.php
+++ application/config/config.php
@@ -379,7 +379,7 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
  */
-$config['sess_driver']             = sys_get_temp_dir(); //'files';
+$config['sess_driver']             = "database"; //'files';
 $config['sess_cookie_name']        = 'ci_session';
 $config['sess_expiration']         = 7200;
 $config['sess_save_path']          = null;
