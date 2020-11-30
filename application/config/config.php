0ed722ccc82361e2f9348568f84312f251d07534
eMul <e2016141010@gmail.com>
2020-12-01 00:34:57 +0700

    beneran fix dah ini mh

--- application/config/config.php
+++ application/config/config.php
@@ -379,10 +379,10 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
  */
-$config['sess_driver']             = null; //'files';
+$config['sess_driver']             = "database"; //'files';
 $config['sess_cookie_name']        = 'ci_session';
 $config['sess_expiration']         = 7200;
-$config['sess_save_path']          = null;
+$config['sess_save_path']          = 'ci_session';
 $config['sess_match_ip']           = false;
 $config['sess_time_to_update']     = 300;
 $config['sess_regenerate_destroy'] = false;
